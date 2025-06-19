65f81c53fde5fb8d129ecb4f3387bfc02e05c96b
f1rtxz <haidaianton@gmail.com>
2025-06-19 21:58:54 +0300

    configure zsh

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -1,5 +1,30 @@
+{ pkgs, ... }: 
+
 {
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    zplug = {
+      enable = true;
+      plugins = [
+      {name = "zsh-users/zsh-autosuggestions";}
+      {
+        name = "romkatv/powerlevel10k";
+        tags = [ "as:theme" "depth:1" ];
+      }
+     ];
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+	"git"
+      ];
+    };
+    initContent = ''
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    '';
   };
 }
