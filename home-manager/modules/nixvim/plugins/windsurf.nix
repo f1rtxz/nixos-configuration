d9a941409a775e3714d7c7bd1af1fc38fc51c142
f1rtxz <haidaianton@gmail.com>
2025-06-20 18:31:19 +0300

    windsurf ai added in nvim

--- home-manager/modules/nixvim/plugins/windsurf.nix
+++ home-manager/modules/nixvim/plugins/windsurf.nix
@@ -1,3 +1,21 @@
 {
-  programs.nixvim = {};
+  programs.nixvim = {
+    plugins = {
+      windsurf-nvim = {
+        enable = true;
+        settings = {
+          enable_cmp_source = false;
+          virtual_text = {
+            enabled = true;
+            manual = false;
+            virtual_text_priority = 65535;
+            map_keys = true;
+            key_bindings = {
+              accept = "<Tab>";
+            };
+          }; 
+        };
+      };
+    };
+  };
 }
