2809168ca42adc38abc0a044a4b04102ac9df381
f1rtxz <haidaianton@gmail.com>
2025-06-20 19:41:39 +0300

    shellAliases added

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -23,6 +23,17 @@
 	      "git"
       ];
     };
+    shellAliases = 
+    let
+      flakeDir = "~/nixos-config"; 
+    in {
+      nrs = "sudo nixos-rebuild switch --flake ${flakeDir}";
+      upd = "cd ${flakeDir} && nix flake update && sudo nixos-rebuild switch --flake ${flakeDir}";
+      hms = "export NIXPKGS_ALLOW_UNFREE=1 && home-manager switch --impure --flake ${flakeDir}";
+
+      ff = "fastfetch";
+      v = "nvim";
+    };
     initContent = ''
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
