77eb36214f6284ae0523af7bd9fe77ffb2dda565
f1rtxz <haidaianton@gmail.com>
2025-06-20 15:50:45 +0300

    lsp added

--- home-manager/modules/nixvim/plugins/lsp.nix
+++ home-manager/modules/nixvim/plugins/lsp.nix
@@ -1,3 +1,21 @@
 {
-  programs.nixvim = {};
+  programs.nixvim = {
+    plugins = {
+      lsp = {
+        enable = true;
+        servers = {
+          pyright.enable = true;
+          ts_ls.enable = true;
+          clangd.enable = true;
+        };
+        keymaps = {
+          silent = true;
+          lspBuf = {
+            gd = "definition";
+            K = "hover";
+          };
+        };
+      };
+    };
+  };
 }
