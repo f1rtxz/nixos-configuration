596a69c4dcf5c822b2ab680b547caee28c5e89d3
f1rtxz <haidaianton@gmail.com>
2025-07-06 13:46:54 +0300

    kitty configuration with nix

--- home-manager/modules/kitty/kitty.nix
+++ home-manager/modules/kitty/kitty.nix
@@ -1,6 +1,6 @@
 {
   programs.kitty = {
     enable = true;
-    extraConfig = builtins.readFile ./kitty.conf;
+    extraConfig = builtins.readFile ./themes/GruvBox_DarkHard.conf + "\n" + builtins.readFile ./kitty.conf;
   };
 }
